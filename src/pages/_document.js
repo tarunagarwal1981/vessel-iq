e8c540f14bde091b16e9977a205fee1b110c9e1a
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-05 19:10:42 +0530

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,16 @@
+// src/pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
