9481403a5e1be39755b9fbe10a9f588183725e0d
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-06 09:47:12 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
